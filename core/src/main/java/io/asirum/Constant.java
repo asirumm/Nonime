ea16747642953ed678bf98b5d57fb8678f3568b6
haslam <haslamhdz71@gmail.com>
2025-05-12 08:57:51 +0700

    setup

---
+++ core/src/main/java/io/asirum/Constant.java
@@ -0,0 +1,7 @@
+package io.asirum;
+
+public class Constant {
+    // Aspek rasio 16:9
+    public static int VIRTUAL_WIDTH = 240;
+    public static int VIRTUAL_HEIGHT = 135;
+}
--- core/src/main/java/io/asirum/Main.java
+++ core/src/main/java/io/asirum/Main.java
@@ -6,29 +6,11 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import de.eskalon.commons.core.ManagedGame;
+import de.eskalon.commons.screen.ManagedScreen;
+import de.eskalon.commons.screen.transition.ScreenTransition;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+public class Main extends ManagedGame<ManagedScreen, ScreenTransition> {
 
-    @Override
-    public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-    }
-
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
-    }
 }
