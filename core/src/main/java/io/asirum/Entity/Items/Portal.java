de26a01b0ec824ac207669f77baf27a1b5958e73
haslam <haslamhdz71@gmail.com>
2025-05-14 12:17:34 +0700

    portal entity

---
+++ core/src/main/java/io/asirum/Entity/Items/Portal.java
@@ -0,0 +1,56 @@
+package io.asirum.Entity.Items;
+
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import io.asirum.Box2d.*;
+import io.asirum.TmxMap.TmxHelper;
+
+public class Portal extends BaseBox2d {
+    private Key key;
+
+    public Portal(World world,Key key) {
+        super(world);
+        this.key = key;
+    }
+
+    public boolean isPlayerCanFinish(){
+        return key.isCollected();
+    }
+
+    @Override
+    public void build(MapObject object) {
+        Rectangle rect = TmxHelper.convertRectangleMapObject(object);
+
+        Vector2 position = Box2dHelper.positionBox2d(rect);
+        Vector2 size     = Box2dHelper.sizeBox2d(rect);
+
+        BodyBuilder bodyBuilder = new BodyBuilder()
+            .fixRotation(true)
+            .position(position.x, position.y);
+
+        body = world.createBody(bodyBuilder.build());
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(size.x,size.y);
+
+        FixtureBuilder fixtureBuilder = new FixtureBuilder()
+            .shape(shape)
+            .filter(FixtureFilter.filterPortal())
+            .isSensor(true);
+
+        Fixture fixture = body.createFixture(fixtureBuilder.build());
+
+        fixture.setUserData(Box2dHelper.PORTAL_FIXTURE_NAME);
+        body.setUserData(this);
+
+        shape.dispose();
+    }
+
+    public Key getKey() {
+        return key;
+    }
+}
