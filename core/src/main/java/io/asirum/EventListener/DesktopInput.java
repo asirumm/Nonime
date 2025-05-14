78582d5ed2ee8ac7bd234d0a5e00366a63deece5
haslam <haslamhdz71@gmail.com>
2025-05-14 12:18:06 +0700

    input controller

--- core/src/main/java/io/asirum/EventListener/DesktopInput.java
+++ core/src/main/java/io/asirum/EventListener/DesktopInput.java
@@ -1,4 +1,22 @@
 package io.asirum.EventListener;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import io.asirum.Entity.Player.PlayerMovement;
+
 public class DesktopInput {
+
+    public static void playerMovementController(){
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            PlayerMovement.moveRight = true;
+        }
+
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            PlayerMovement.moveLeft = true;
+        }
+
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+            PlayerMovement.moveUp = true;
+        }
+    }
 }
--- core/src/main/java/io/asirum/EventListener/MobileInput.java
+++ core/src/main/java/io/asirum/EventListener/MobileInput.java
@@ -1,4 +1,24 @@
 package io.asirum.EventListener;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import io.asirum.Entity.Player.PlayerMovement;
+
 public class MobileInput {
+    public static void leftControl(Button button){
+        button.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                PlayerMovement.moveLeft = true;
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                PlayerMovement.moveLeft = false;
+            }
+        });
+    }
+
 }
