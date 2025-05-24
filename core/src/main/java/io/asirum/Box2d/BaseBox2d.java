7bd44ab9ce5766c7e586b8780129edd8c1a1084a
haslam <haslamhdz71@gmail.com>
2025-05-24 16:27:59 +0700

    Refactor

--- core/src/main/java/io/asirum/Box2d/BaseBox2d.java
+++ core/src/main/java/io/asirum/Box2d/BaseBox2d.java
@@ -1,9 +1,12 @@
 package io.asirum.Box2d;
 
 import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
+import io.asirum.Constant;
 
 public abstract class BaseBox2d {
     protected World world;
@@ -30,5 +33,22 @@ public abstract class BaseBox2d {
 
     public World getWorld(){
         return world;
+    }
+
+
+    /**
+     */
+    public Vector2 positionBox2d(Rectangle rect){
+        float x = (rect.x + rect.width / 2f) / Constant.UNIT_SCALE;
+        float y = (rect.y + rect.height / 2f) / Constant.UNIT_SCALE;
+
+        return new Vector2(x,y);
+    }
+
+    public Vector2 sizeBox2d(Rectangle rect){
+        float halfWidth = rect.width / 2f / Constant.UNIT_SCALE;
+        float halfHeight = rect.height / 2f / Constant.UNIT_SCALE;
+
+        return new Vector2(halfWidth,halfHeight);
     }
 }
---
+++ core/src/main/java/io/asirum/Box2d/Box2dVars.java
@@ -0,0 +1,28 @@
+package io.asirum.Box2d;
+
+public class Box2dVars {
+
+    // LAYER (TMX)
+    public static final String PLATFORM_LAYER = "platform";
+    public static final String ENTITIES_LAYER = "objects";
+    public static final String ONE_WAY_PLATFORM_LAYER = "one-way-platform";
+    public static final String CHECKPOINT_LAYER = "checkpoint";
+    public static final String OBSTACLE_LAYER = "obstacle";
+
+    // OBJECT NAME (TMX)
+    public static final String PLAYER_NAME = "player";
+    public static final String PLAYER_SENSOR_NAME = "foot-sensor";
+    public static final String KEY_SENSOR_NAME = "key-sensor";
+    public static final String PORTAL_SENSOR_NAME = "portal-sensor";
+    public static final String OBSTACLE_NAME = "obstacle";
+
+    // FIXTURE
+    public static final String PLAYER_SENSOR_FIXTURE = "foot";
+    public static final String STATIC_PLATFORM_FIXTURE = "static_platform";
+    public static final String ONE_WAY_PLATFORM_FIXTURE = "one-way-platform";
+    public static final String PLAYER_FIXTURE = "player";
+    public static final String KEY_FIXTURE = "key";
+    public static final String CHECKPOINT_FIXTURE = "checkpoint";
+    public static final String PORTAL_FIXTURE = "portal-sensor";
+
+}
