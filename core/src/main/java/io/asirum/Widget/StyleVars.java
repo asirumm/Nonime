973dc76a6941b8cfe2a2c4d17fde3126a1cce975
haslam <haslamhdz71@gmail.com>
2025-05-27 19:45:21 +0700

    menambahkan variable style widget

--- core/src/main/java/io/asirum/Widget/StyleVars.java
+++ core/src/main/java/io/asirum/Widget/StyleVars.java
@@ -5,4 +5,15 @@ public class StyleVars {
     public static final String ABOUT_BTN = "about";
     public static final String PLAY_BTN = "";
     public static final String HOME_BACKGROUND = "";
+    public static final String PAUSE_BUTTON = "pause";
+    public static final String HOME_BUTTON = "home";
+    public static final String LEFT_CONTROL = "left";
+    public static final String RIGHT_CONTROL = "right";
+    public static final String MOBILE_CONTROL_LEFT = "control-left";
+    public static final String MOBILE_CONTROL_RIGHT = "control-right";
+    public static final String MOBILE_CONTROL_UP = "control-up";
+    public static final String TITLE_LIGHT_LABEL = "title-light";
+    public static final String COST_TEXT_BUTTON = "cost";
+    public static final String LEVEL_TEXT_BUTTON = "level";
+    public static final String MUSIC_BTN = "music";
 }
