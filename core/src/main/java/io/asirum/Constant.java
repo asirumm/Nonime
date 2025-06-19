2e9d7d0cf1f5712faf502ea4dd514204c8e96613
haslam <haslamhdz71@gmail.com>
2025-06-19 19:09:59 +0700

    penambahan aset yang di load

--- core/src/main/java/io/asirum/Constant.java
+++ core/src/main/java/io/asirum/Constant.java
@@ -2,8 +2,8 @@ package io.asirum;
 
 public class Constant {
     // Aspek rasio 16:9
-    public static int VIRTUAL_WIDTH = 320;
-    public static int VIRTUAL_HEIGHT = 180;
+    public static int VIRTUAL_WIDTH = 480;
+    public static int VIRTUAL_HEIGHT = 270;
 
     public static float UNIT_SCALE = 16f;
 
@@ -19,11 +19,15 @@ public class Constant {
 
     // music
     public static String ASSET_SOUND_CONTROL_ON_CLICK = "manifest/audio/control-level.ogg";
-    public static String ASSET_SOUND = "manifest/audio/a-big-world.ogg";
+    public static String ASSET_SOUND = "manifest/audio/Forest.ogg";
+    public static String ASSET_KEY = "manifest/audio/key.ogg";
+    public static String ASSET_DIE = "manifest/audio/die.ogg";
 
     // font
     public static String ASSET_FONT_FNT = "manifest/font/m5x7-16.fnt";
     public static String ASSET_FONT_IMAGE = "manifest/font/font.png";
+
+    public static String BACKGROUND_ATLAS = "manifest/background/background.atlas";
 
     public static final String GAME_DATA_JSON = "game-data/payload.json";
     public static final String USER_DATA_PREFERENCES = "user-pref";
