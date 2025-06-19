1b93744b53b0efd9c10c7870387f6be81e95af4f
haslam <haslamhdz71@gmail.com>
2025-06-19 19:07:12 +0700

    menambahkan asset

---
+++ assets/manifest/map/forest/enviroment.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="enviroment" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="544" columns="34">
+ <image source="enviroment.png" width="612" height="288"/>
+</tileset>
